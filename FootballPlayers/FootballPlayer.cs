a6a8196be881b43bfe0152ead9fdf557e99e365c
Muhammad Adeel <muha4203@edu.zealand.dk>
2022-10-06 09:43:22 +0200

    Add project files.

---
+++ FootballPlayers/FootballPlayer.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+
+namespace FootballPlayers
+{
+    public class FootballPlayer
+    {
+        public int Id { get; set; }
+        public int ShirtNo { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+        public void ValidateName()
+        {
+
+            if (Name.Length < 2)
+                throw new ArgumentOutOfRangeException("Name must contain two character" + Name);
+
+
+        }
+
+        public override string ToString()
+        {
+            return Id + "," + Name + "," + Age + "," + ShirtNo;
+        }
+        public void ValidateAge()
+        {
+            if (Age < 1)
+                throw new ArgumentOutOfRangeException("Age Must be above one" + Age);
+        }
+        public void ValidateShirtNo()
+        {
+            if (ShirtNo < 1 || ShirtNo>99)
+             throw new ArgumentOutOfRangeException("ShirtNo cannot be less than 1" + ShirtNo);
+           
+
+        }
+
+        public void Validate()
+        {
+            ValidateAge();
+            ValidateName();
+            ValidateShirtNo();  
+        }
+
+
+    }
+
+
+
+
+
+}
---
+++ FootballPlayersTests/FootballPlayerTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FootballPlayers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+
+namespace FootballPlayers.Tests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+        private FootballPlayer footballPlayer = new FootballPlayer { Id = 1,Name= "Eriksen", Age = 30, ShirtNo = 14 };
+        private FootballPlayer footballPlayerneagtiveAge = new FootballPlayer { Id = 2, Name = "Ronaldo", Age = -1, ShirtNo = 7 };
+        private FootballPlayer footballPlayername = new FootballPlayer { Id = 3, Name = "A", Age = 30, ShirtNo = 11 };
+        private FootballPlayer footballPlayerShirtNo = new FootballPlayer { Id = 4, Name = "Messi", Age = 30, ShirtNo = 111 };
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            string str = footballPlayer.ToString();   // act
+            Assert.AreEqual("1,Eriksen,30,14", str);  // assert
+        }
+        [TestMethod()]
+        public void ValidateNegativeAgeTest()
+        {
+           // footballPlayer.ValidateAge();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => footballPlayerneagtiveAge.ValidateAge());
+        }
+        [TestMethod()]
+        public void ValidatePlayerName()
+        {
+            //footballPlayer.ValidateName();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => footballPlayername.ValidateName());
+        }
+        [TestMethod()]
+         public void ValidatePlayerShirtNumber()
+        {
+           // footballPlayer.ValidateShirtNo();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => footballPlayerShirtNo.ValidateShirtNo());
+           
+
+        }
+        //[TestMethod()]
+        //public void ValidateTest()
+        //{
+        //    footballPlayer.Validate();
+        //    Assert.ThrowsException<ArgumentOutOfRangeException>(() => footballPlayerneagtiveAge.Validate());
+        //    Assert.ThrowsException<ArgumentOutOfRangeException>(() => footballPlayername.Validate());
+        //    Assert.ThrowsException<ArgumentOutOfRangeException>(() => footballPlayerShirtNo.Validate());
+        //}
+           
+
+        [TestMethod()]
+        [DataRow(2)]
+        [DataRow(100)]
+        [DataRow(14593)]
+        public void ValidaAgeTest(int age)
+        {
+            footballPlayer.Age = age ;
+            footballPlayer.ValidateAge();
+        }
+       
+        public void ValidaShirtNo(int shirtnumber)
+        {
+            footballPlayer.ShirtNo= shirtnumber;
+            footballPlayer.ValidateShirtNo();
+           
+        }
+        public void ValidateName(string name)
+        {
+            footballPlayer.Name = name; 
+            footballPlayer.ValidateName();  
+        }
+
+    }
+}
